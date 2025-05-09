63bb8aeac030f50a371eebd1f28095ba36a39555
chintabhavitha <chintabhavitha@gmail.com>
2025-05-09 21:45:59 +0530

    Assignment15

--- src/assignments/Assignment14_DiamondShapedPyramid.java
+++ src/assignments/Assignment14_DiamondShapedPyramid.java
@@ -7,15 +7,21 @@ public class Assignment14_DiamondShapedPyramid {
 	public static void main(String[] args) {
 
 		int n= 5;
+		// upper pyramid
+		// to iterate numbers from 1 to 5
 		for(int i =1; i <=n; i++) {
+			// to print spaces
 			for(int j =n; j>i; j--) {
 				System.out.print(" ");
 			}
+			//to print numbers
 			for(int k = 1; k<=i; k++) {
 				System.out.print(k + " ");
 			}
 			System.out.println();
 		}
+		//lower pyramid
+		//to iterate numbers from 4 to 1
 		for(int i = n-1; i >=1; i--) {
 			for (int j = n; j>i; j--) {
 				System.out.print(" ");
--- src/assignments/Assignment15_StringOps.java
+++ src/assignments/Assignment15_StringOps.java
@@ -3,11 +3,27 @@ package assignments;
 public class Assignment15_StringOps {
 
 	public static void main(String[] args) {
-		
-		String sentence = "Java programming is fun and challenging";
-		
-		//1. Count the total number of words in the sentence.
 
+		String sentence = "Java programming is fun and challenging";
+
+		// 1. Count the total number of words in the sentence.
+		
+		System.out.println(sentence);
+		String[] words = sentence.trim().split("\\s+"); // (\\s+) it splits the sentence at one or more white spaces
+		System.out.println(words.length);
+		
+
+		
+		//2.Print the sentence words in reverse order.
+		System.out.println(sentence);
+		
+		for(int i=words.length-1; i>=0; i--) {
+			System.out.print(words[i] + " ");
+		}
+		
+		//3.Convert the first character of each word to uppercase and print original sentence
+		
+		
 	}
 
 }
